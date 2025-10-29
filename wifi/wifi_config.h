1b256e3a749f54b4d5d39ea5d84ec5fb83402a41
Shi Min <k.shimin20@gmail.com>
2025-10-29 17:33:38 +0800

    feat: Pico W connection to WiFi and obtaining IP addresses.

---
+++ wifi/wifi_config.h
@@ -0,0 +1,5 @@
+#define WIFI_SSID "SM"
+#define WIFI_PASSWORD "shimin0323"
+#define GATEWAY_IP "192.168.56.1"  // Your laptop's IP address (found using `ipconfig /all` for windows)
+#define GATEWAY_PORT 1883
+#define CLIENT_ID "pico_w_client"
