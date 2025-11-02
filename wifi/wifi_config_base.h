25ae62f8b7f5be38ff326b7987a003df51722b0c
Shi Min <k.shimin20@gmail.com>
2025-11-02 23:42:37 +0800

    refactor: update `wifi_config_base.h` gateway port to match intented port.

--- wifi/wifi_config_base.h
+++ wifi/wifi_config_base.h
@@ -1,5 +1,5 @@
 #define WIFI_SSID "YOUR_WIFI_NAME"
 #define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
 #define GATEWAY_IP "YOUR_LAPTOP_IP"  // Your laptop's IP address (found using `ipconfig /all` for windows)
-#define GATEWAY_PORT 1883
+#define GATEWAY_PORT 1884
 #define CLIENT_ID "pico_w_client"
